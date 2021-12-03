9123f4d34fc9933994bc7093332c26086ac9b19c
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-12-03 12:34:04 -0500

    Code push

---
+++ OdeToFood.Data/Services/InMemoryRestaurantDate.cs
@@ -0,0 +1,28 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantDate : IRestaurantData
+    {
+
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantDate()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                new Restaurant {Id = 3, Name = "Mango Grove", Cuisine = CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
